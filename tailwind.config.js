b28c00953eb2a9911127232fad5968cc3328513c
Terry Wambui <shikuwambui@gmail.com>
2024-02-12 17:05:08 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.{html, js}", 
+    "./public/**/*"
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
